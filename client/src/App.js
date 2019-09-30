2ad53485001e0d8583b774613b67fc6edcbf77d6
egrinberg <46496977+egrinberg@users.noreply.github.com>
2019-09-30 12:35:59 -0400

    User Model + signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,6 @@
 import React, {Component} from 'react';
 import AppNavbar from './components/AppNavbar'
-<<<<<<< HEAD
-import ShoppingList from './components/ShoppingList'
-
-=======
 import Home from './components/Home'
->>>>>>> f6d45bb68b25b933934f89d3d7e30765e8f1b532
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
