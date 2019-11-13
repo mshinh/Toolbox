3e5172c693e2f46ae446010b26fafc14c578944a
egrinberg <46496977+egrinberg@users.noreply.github.com>
2019-11-13 14:44:40 -0500

    post display

---
+++ client/src/components/PostDisplay/index.js
@@ -0,0 +1,170 @@
+import React, { Component } from "react";
+import Post from "./Post";
+import "./style.scss";
+
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { logout } from "../../actions/auth";
+
+const PostDisplay = ({ auth: { isAuthenticated, loading } }) => {
+  return isAuthenticated ? (
+    <div className="home-container">
+      <Post currPost={this.state.currPost} />
+
+      <div className="page-heading">
+        <h1>Welcome To Toolbox</h1>
+        <h2>
+          Your one stop location for <br />
+          skilled workers and opportunities{" "}
+        </h2>
+      </div>
+      {/* Make this a class */}
+      <div className="post-template">
+        <div className="post-popup">
+          {/* This has to be moved becasue it will take up the entire home page */}
+        </div>
+        <div className="post-board">{this.createPosts()}</div>
+      </div>
+    </div>
+  ) : (
+    <div className="home-container">
+      <Post currPost={this.state.currPost} />
+
+      <div className="page-heading">
+        <h1>Welcome To Toolbox</h1>
+        <h2>
+          Your one stop location for <br />
+          skilled workers and opportunities{" "}
+        </h2>
+      </div>
+      {/* Make this a class */}
+      <div className="post-template">
+        <div className="post-popup">
+          {/* This has to be moved becasue it will take up the entire home page */}
+        </div>
+        <div className="post-board">{this.createPosts()}</div>
+      </div>
+    </div>
+  );
+};
+
+PostDisplay.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(PostDisplay);
+
+// class PostDisplay extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       currPost: { id: 0, type: "", title: "", desc: "" }
+//     };
+//     this.createPosts = this.createPosts.bind(this);
+//     this.currentSettings = this.currentSetting.bind(this);
+//     this.activeContent = this.activeContent.bind(this);
+//   }
+
+//   currentSetting = () => {
+//     let currPath = this.props.location.pathname;
+//     return currPath.substr(1);
+//   };
+//   createPosts = () => {
+//     let post = {
+//       id: 0,
+//       type: "Job Posting",
+//       title: "Home Page Testing",
+//       desc:
+//         " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tincidunt viverra nulla ut mattis. Quisque tincidunt pretium lorem at porttitor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris malesuada tempus maximus. Suspendisse tincidunt consectetur tortor vitae aliquam. Suspendisse auctor tortor ut magna iaculis, id venenatis mi facilisis. "
+//     };
+//     let Accountpost = {
+//       id: 0,
+//       type: "Job Posting",
+//       title: "Account Page Posting",
+//       desc:
+//         " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tincidunt viverra nulla ut mattis. Quisque tincidunt pretium lorem at porttitor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris malesuada tempus maximus. Suspendisse tincidunt consectetur tortor vitae aliquam. Suspendisse auctor tortor ut magna iaculis, id venenatis mi facilisis. "
+//     };
+//     let posts = [];
+//     let postData = [];
+//     console.log(this.currentSetting());
+
+//     if (this.currentSetting() === "account") {
+//       //On Account Page
+//       postData.push(Accountpost);
+//     } else {
+//       //On Home Page
+//       postData.push(post);
+//     }
+
+//     //Add a check to see if the user has posted anything
+
+//     if (postData.length > 0) {
+//       postData.map((data, index) => {
+//         posts.push(this.postContent(data, index));
+//       });
+//     }
+
+//     return posts;
+//   };
+
+//   postContent = (data, idx) => {
+//     let post = (
+//       <div key={idx} className="post">
+//         <h4>{data.type}</h4>
+//         <h3>{data.title}</h3>
+//         <p>{data.desc}</p>
+//         <div className="buttom-container">
+//           <button
+//             type="submit"
+//             onClick={e => {
+//               this.activeContent(data);
+//             }}
+//             className="input-btn"
+//           >
+//             <h4>More Information</h4>
+//             <span className="button-bar"></span>
+//           </button>
+//         </div>
+//       </div>
+//     );
+
+//     return post;
+//   };
+
+//   activeContent = data => {
+//     console.log("update Content");
+//     this.setState({
+//       currPost: data
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div className="home-container">
+//         <Post currPost={this.state.currPost} />
+
+//         <div className="page-heading">
+//           <h1>Welcome To Toolbox</h1>
+//           <h2>
+//             Your one stop location for <br />
+//             skilled workers and opportunities{" "}
+//           </h2>
+//         </div>
+//         {/* Make this a class */}
+//         <div className="post-template">
+//           <div className="post-popup">
+//             {/* This has to be moved becasue it will take up the entire home page */}
+//           </div>
+//           <div className="post-board">{this.createPosts()}</div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default PostDisplay;
