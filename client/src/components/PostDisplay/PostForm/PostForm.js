2fc61e6c691d735ed1294c0722a3cb27028eb9e5
WIN-V0JMUPNT1RG\Maxim <maxim124@live.com>
2019-11-15 07:40:01 -0500

    Styling update

---
+++ client/src/components/PostDisplay/PostForm/PostForm.js
@@ -0,0 +1,145 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { addPost } from "../../../actions/post";
+
+import "./style.scss";
+
+const PostForm = ({ addPost }) => {
+  const [formPost, setPostData] = useState({
+    title: "",
+    body: "",
+    location: "",
+    type: ""
+  });
+
+  const [active, activeToggle] = useState(false)
+
+
+  const { title, body, location, type } = formPost;
+
+  const onChange = e => {
+    setPostData({ ...formPost, [e.target.name]: e.target.value });
+  };
+
+
+
+  const onSubmit = e => {
+    e.preventDefault();
+    addPost(formPost);
+    setPostData({
+      title: "",
+      body: "",
+      location: "",
+      type: "job"
+    });
+  };
+
+  return (
+    <div className={`post-form-container ${active ? "active" : "notActive"}`}>
+   
+               
+        <button className="input-btn" onClick={() => activeToggle(!active)}>
+                    <h4>Toggle Post Creator</h4>  
+                    <span className="button-bar"></span>  
+          </button> 
+
+      
+            
+    <form className="addBarForm" onSubmit={e => onSubmit(e)}>
+    <div className="form-wrapper">
+        <div className="form-row">
+           
+       
+
+        <fieldset className="form-column" id='meta-form'>
+        <h2 className="input-heading">Create A Post</h2>
+          <div className="input-row">
+            <div className="input-wrapper">
+              <label htmlFor="type">Type</label>
+              {/* Have to test select with onChange method */}
+              {/* How to set default value with select? */}
+              <select
+                name="type"
+                id="type"
+                placeholder="Select Job"
+                value={type}
+                onChange={e => onChange(e)}
+              >
+                <option value="job">Job</option>
+                <option value="general">General</option>
+                <option value="other">other</option>
+              </select>
+            </div>
+          </div>
+          <div className="input-row">
+            <div className="input-wrapper">
+              <label htmlFor="title">Title</label>
+
+              {/* This will have to be broken down into street name, city name etc... next semester */}
+              <input
+                name="title"
+                type="text"
+                id="title"
+                placeholder="Require Plumber"
+                onChange={e => onChange(e)}
+                value={title}
+              />
+            </div>
+          </div>
+          <div className="input-row">
+            <div className="input-wrapper">
+              <label htmlFor="location">Location</label>
+
+              {/* This will have to be broken down into street name, city name etc... next semester */}
+              <input
+                name="location"
+                type="text"
+                id="location"
+                placeholder="69 Street Ave"
+                onChange={e => onChange(e)}
+                value={location}
+              />
+            </div>
+          </div>
+          <div className="input-row">
+            <div className="input-wrapper">
+              <label htmlFor="description">Description</label>
+              {/* Maybe not have this at all, have to think about it*/}
+              <textarea
+                row="10"
+                col="80"
+                name="body"
+                type="text"
+                id="body"
+                value={body}
+                placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit.  "
+                onChange={e => onChange(e)}
+              >
+                {body}
+              </textarea>
+            </div>
+          </div>
+
+          </fieldset>
+        </div>
+{/*         
+        <button type='submit' className="input-btn" onClick={this.onSubmit}>
+                    <h4>Submit Post</h4>  
+                    <span className="button-bar"></span>  
+          </button> */}
+
+
+          <input type="submit" className="btn btn-dark my-1" value="Post" />
+          
+      </div>
+      </form>
+    </div>
+  );
+};
+
+PostForm.propTypes = {
+  addPost: PropTypes.func.isRequired
+};
+
+export default connect(null, { addPost })(PostForm);
