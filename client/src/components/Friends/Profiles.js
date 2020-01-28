cd575129c51732ea660f27b156c282f1b3948a56
Maxim <maxim124@livec.com>
2020-01-27 19:15:52 -0500

    The contact list is updated in display

--- client/src/components/Friends/Profiles.js
+++ client/src/components/Friends/Profiles.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import "./style.scss";
 import { connect } from "react-redux";
@@ -6,6 +6,7 @@ import Spinner from "../Layout/Spinner";
 import ProfileItem from "./ProfileItem";
 
 import { getProfiles } from "../../actions/profile";
+import ContactDisplay from "./ContactDisplay";
 
 const Profiles = ({
   getProfiles,
@@ -16,6 +17,15 @@ const Profiles = ({
     getProfiles();
   }, []);
 
+  const updateActive = (profile) => {
+    console.log(profile);
+  }
+  const [activeDisplay, setDisplay] = useState({
+    profile: {},
+  });
+
+
+  
   return (
     <Fragment>
       {loading ? (
@@ -23,19 +33,20 @@ const Profiles = ({
       ) : (
         <Fragment>
           <div className="friend-container">
-            <h2>Friends Center</h2>
+            <h2>Contact Center</h2>
             <div className="friend-list">
               {profiles.length > 0 ? (
                 profiles.map(profile => (
-                  <ProfileItem key={profile._id} profile={profile} />
+                      <ProfileItem key={profile._id}  profile={profile} />
                 ))
               ) : (
-                <h4>No friends found</h4>
+                <h4>No Contacts Found</h4>
               )}
             </div>
-            <div>
-              <h2> Close Friends </h2>
-            </div>
+          
+          </div>
+          <div className="displayContainer">
+                <ContactDisplay activeDisplay={activeDisplay} />
           </div>
         </Fragment>
       )}
